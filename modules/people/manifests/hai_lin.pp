1c75eb64b396553beb59fe9037b2f052dfbe04cb
Hai Lin <hai.lin.cs@gmail.com>
2015-05-11 15:54:15 -0700

    Remove some install in personal

--- modules/people/manifests/hai_lin.pp
+++ modules/people/manifests/hai_lin.pp
@@ -1,15 +1,12 @@
 class people::hai_lin{
     include atom
-    include chrome
     include clojure
     include dropbox
     include evernote
-    include firefox
     include flux
     include github_for_mac
     include imagemagick
     include iterm2::stable
-    include java
     include skitch
     include macvim
     include spotify
